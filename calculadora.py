29d2700c06493b1e78ed12c0557662cdfbeaad06
Capitionex <luizflpcapi@gmail.com>
2022-08-11 15:57:17 -0500

    Primeira alteração

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,17 @@
 number_1 = int (input ('Enter your first number: '))
 number_2 = int (input ('Enter your second number: '))
 
-print (f'{number_1} + {number_2} = number_1+ number_2))
+print (f'{number_1} + {number_2} =' number_1+ number_2))
 print (number_1 + number_2)
+
+#subtração
+print ('{} - {} = '.format(number_1,number_2))
+print (number_1-number_2)
+
+#multipliação
+print ('{} - {} = '.format(number_1,number_2))
+print (number_1*number_2)
+
+#divisão
+print ('{} - {} = '.format(number_1,number_2))
+print (number_1/number_2)
