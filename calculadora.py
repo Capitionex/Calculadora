cdf8a404d1ef39a3a659424ca9034c119505f13f
Capitionex <luizflpcapi@gmail.com>
2022-08-11 16:02:39 -0500

    #2 adicionar os operadores básico (subtração, multiplicação e divisão

--- calculadora.py
+++ calculadora.py
@@ -1,17 +1,29 @@
-number_1 = int (input ('Enter your first number: '))
-number_2 = int (input ('Enter your second number: '))
+operation = input('''
+Please type in the math operation you would like to complete:
++ for addition
+- for subtraction
+* for multiplication
+/ for division
+''')
 
-print (f'{number_1} + {number_2} =' number_1+ number_2))
-print (number_1 + number_2)
+number_1 = int(input('Enter your first number: '))
+number_2 = int(input('Enter your second number: '))
 
-#subtração
-print ('{} - {} = '.format(number_1,number_2))
-print (number_1-number_2)
+if operation == '+':
+    print('{} + {} = '.format(number_1, number_2))
+    print(number_1 + number_2)
 
-#multipliação
-print ('{} - {} = '.format(number_1,number_2))
-print (number_1*number_2)
+elif operation == '-':
+    print('{} - {} = '.format(number_1, number_2))
+    print(number_1 - number_2)
 
-#divisão
-print ('{} - {} = '.format(number_1,number_2))
-print (number_1/number_2)
+elif operation == '*':
+    print('{} * {} = '.format(number_1, number_2))
+    print(number_1 * number_2)
+
+elif operation == '/':
+    print('{} / {} = '.format(number_1, number_2))
+    print(number_1 / number_2)
+
+else:
+    print('You have not typed a valid operator, please run the program again.')
