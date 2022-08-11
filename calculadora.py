8c68138f6a99c5f80a362fcdcce89fb7582c50e0
Capitionex <luizflpcapi@gmail.com>
2022-08-11 16:13:54 -0500

    #4 o usuário deverá realizar novo calculo sem executar a aplicação novamente

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,6 @@
-operation = input('''
+# Define our function
+def calculate():
+    operation = input('''
 Please type in the math operation you would like to complete:
 + for addition
 - for subtraction
@@ -6,24 +8,27 @@ Please type in the math operation you would like to complete:
 / for division
 ''')
 
-number_1 = int(input('Digite o primeiro número: '))
-number_2 = int(input('Digite o segundo número: '))
+    number_1 = int(input('Please enter the first number: '))
+    number_2 = int(input('Please enter the second number: '))
 
-if operation == '+':
-    print('{} + {} = '.format(number_1, number_2))
-    print(number_1 + number_2)
+    if operation == '+':
+        print('{} + {} = '.format(number_1, number_2))
+        print(number_1 + number_2)
 
-elif operation == '-':
-    print('{} - {} = '.format(number_1, number_2))
-    print(number_1 - number_2)
+    elif operation == '-':
+        print('{} - {} = '.format(number_1, number_2))
+        print(number_1 - number_2)
 
-elif operation == '*':
-    print('{} * {} = '.format(number_1, number_2))
-    print(number_1 * number_2)
+    elif operation == '*':
+        print('{} * {} = '.format(number_1, number_2))
+        print(number_1 * number_2)
 
-elif operation == '/':
-    print('{} / {} = '.format(number_1, number_2))
-    print(number_1 / number_2)
+    elif operation == '/':
+        print('{} / {} = '.format(number_1, number_2))
+        print(number_1 / number_2)
 
-else:
-    print('You have not typed a valid operator, please run the program again.')
+    else:
+        print('You have not typed a valid operator, please run the program again.')
+
+# Call calculate() outside of the function
+calculate()
